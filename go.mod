7f3102bd0fc7754fc3d0b3259677ef9e3c1fc071
Tristan Leo <tleo+git@sipgate.de>
2021-07-12 21:06:53 +0200

    first working version

---
+++ go.mod
@@ -0,0 +1,10 @@
+module trello-fast-card
+
+go 1.16
+
+require (
+	github.com/adlio/trello v1.9.0
+	github.com/martinlindhe/inputbox v0.0.0-20210326232244-b26136a79ad0
+	github.com/zalando/go-keyring v0.1.1
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"github.com/adlio/trello"
+	"github.com/martinlindhe/inputbox"
+	"github.com/zalando/go-keyring"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type config struct {
+	AppKey string `yaml:"appKey"`
+	IdList string `yaml:"idList"`
+}
+
+func main() {
+	service := "trello-fast-card"
+	user := "trello-api-key"
+
+	cfg := config{}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("could not resolve home dir %v", err)
+	}
+	configPath := filepath.Join(home, ".config", "trello-fast-card.yaml")
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	log.Println(cfg)
+
+	trelloApiKey, err := keyring.Get(service, user)
+	if err != nil {
+		got, ok := inputbox.InputBox("trello api key", "input trello api key", "")
+		if ok {
+			err := keyring.Set(service, user, got)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			fmt.Println("No value entered")
+		}
+
+		trelloApiKey, err = keyring.Get(service, user)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	client := trello.NewClient(cfg.AppKey, trelloApiKey)
+
+	got, ok := inputbox.InputBox("Card Text", "Input card text", "")
+	if ok {
+		card := trello.Card{
+			Name: got,
+			IDList: cfg.IdList,
+		}
+		err := client.CreateCard(&card, trello.Defaults())
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("No value entered")
+	}
+}
